7fa152761522b8af0e29be609709896840bb10bb
Alejandro Torrico <37125873+alej96@users.noreply.github.com>
2019-04-15 00:16:19 -0500

    Add files via upload

--- Shiny/server.R
+++ Shiny/server.R
@@ -1,22 +1,35 @@
 #-------------------
 #Project Members: Bria Garcia,Alejandro Torrico, Brooke Larkin , Hector Ramos, Ty Matindale
-#Faculty Advisor: Dr. Ed Polh
+#Faculty Advisor: Dr. Ed Pohl
 #Industry Partners: Raegon Barnes and Willie Nelson
 #-------------------
 
 #-------------------
 #Open R Packages
+install.packages("shiny")
+install.packages("readxl")
+install.packages("plotly")
+install.packages("tidyr")
+install.packages("zoo")
+install.packages("shinywidgets")
+install.packages("shinythemes")
 
+
+#library(dfoptim)
+#library(ggplot2)
+#library(slam)
+#library(Rglpk)
+#library(DT)
+
+#library(dplyr)
+#library(shiftR)
 library(shiny)
-library(dfoptim)
-library(ggplot2)
-library(slam)
-library(Rglpk)
-library(DT)
-
-library(dplyr)
-library(shiftR)
 library(readxl)
+library(plotly)
+library(tidyr)
+library("zoo")
+library(shinyWidgets)
+library(shinythemes)
 
 #-------------------
 
@@ -27,11 +40,17 @@ set.seed(5)
 
 #-------------------
 #Point R to all functions
-
-source("Shiny/Functions/Zero_OH_Qty_Method.R")
-source("Shiny/Functions/cleandata.R")
-
-#source("Functions/DescriptiveStats.R")
+source("Shiny/Functions/cleandata2.R")
+source("Shiny/Functions/Zero_OH_Qty_Method.v2.R")
+source("Shiny/Functions/Weighted_OH_Method_v2.R")
+source("Shiny/Functions/Method2.R")
+source("Shiny/Functions/Graphs/Time_Series_Chart.R")
+source("Shiny/Functions/Graphs/Bar_Chart.R")
+source("Shiny/Functions/Graphs/Geo_Map.R")
+source("Shiny/Functions/Graphs/Drilldown_Bar_Chart.R")
+source("Shiny/Functions/Graphs/Cluster_Chart.R")
+source("Shiny/Functions/Graphs/Cluster_Grid.R")
+source("Shiny/Functions/Graphs/Cluster_Data.R")
 
 #-------------------
 
@@ -40,8 +59,8 @@ source("Shiny/Functions/cleandata.R")
 
 server = function(input, output){
   
-  #Increase max limit on file size to 10KB
-  options(shiny.maxRequestSize=30*1024^2) 
+  #Increase max limit on file size to 50MB
+  options(shiny.maxRequestSize=500^1024^2) 
   
   #Wait for Submit Button
   observeEvent(input$submit, {
@@ -50,35 +69,275 @@ server = function(input, output){
     validate(
       need(input$data.in, "No File Selected")
     )
-    req(input$data.in)
-    
-    withProgress(message = "Starting", value = 0,{
 
     
-       
+    withProgress(message = "Starting", value = 0,{
+      
+      req(input$data.in)
+
+      #Table Title
+      output$SalesData <- renderText( 
+        paste("Sales Data Table")
+      )
+      
+      #Pass input file to clean.data
+      #incProgress(1/11, message = "Analyzing Missed Sales Opportunities")
+      req(input$methods)
+      clean_data = clean.data(input$data.in$datapath, input$file_type)
+      
+      mtd_list = list()
+      mtd_list = input$methods
+      
+      nbr_mtds = length(mtd_list)
+      
+      empty_data <- data.frame("UPC" = c(0,0),"Store Nbr"=c(0,0),	"HSY Item Description" = c("empty data","empty data"),
+                              "Building City" =c("empty data", "empty data"),	
+                              "HSY Seasonal Segmentation" =c( "empty data","empty data"),
+                              "WM Date" = c(0,0),	"Store Name" = c("empty data","empty data"),
+                              "Building State/Prov" =c("empty data","empty data"),	
+                              "Building Postal Code"= c("empty data","empty data"),
+                              "SeasonAndYear" = c("empty data","empty data"),
+                              "Unit Retail" =c(0,0) ,"OH Qty" = c(0,0),	
+                              "POS Sales" =c(0,0),	"POS Qty"=c(0,0), "MSO" =c(0,0))
+      
+      col_names = c("UPC","Store Nbr",	"HSY Item Description","Building City",	"HSY Seasonal Segmentation"	, 
+                   "WM Date",	"Store Name",	"Building State/Prov",	"Building Postal Code",	"SeasonAndYear",
+                   "Unit Retail","OH Qty",	"POS Sales",	"POS Qty", "MSO")
+      
+      colnames(empty_data) <- col_names
+      
+      MSO_1 = empty_data
+      MSO_2 = empty_data
+      MSO_3 = empty_data
+      
+      #View(mtd_list)
+    #  View(nbr_mtds)
+      for(i in 1: nbr_mtds){
         
-        #Table Title
-        output$SalesData <- renderText( 
-          paste("Sales Data Table")
-        )
-        
-        #Pass input file to clean.data
-        incProgress(1/11, message = "Analizing Zero OH Method")
-        MSO = Zero_OH_Qty_Method(input$data.in$datapath)
-        output$data_cleaned = renderDataTable({
+        if(mtd_list[[i]] == "mtd1")
+        {
+          MSO_1 = Zero_OH_Qty_Method(clean_data)
+          incProgress(1/11, message = "Making Table")
           
-          MSO
+          #changed from data_cleaned_1
+          output$MSO_table_1 = DT::renderDataTable(
+            DT::datatable(MSO_1, options =list(
+              pageLength = 5, paging = TRUE )))
+          
+          
+          MSO_sum_1 = sum(MSO_1$MSO)
+          
+          #changed from sum_MSO_1 -> didn't fix issue though
+          output$SalesData_1 = renderText({
+            
+            paste("Missed Sales Opportunities: $" , format(round(MSO_sum_1, digits = 0), big.mark = ",") )
           })
-        
-        
-        MSO_sum = sum(MSO$MSO)
-        
-        output$sum_MSO = renderText({
           
-          paste("Missed Sales Opportunities: " , MSO_sum )
-        })
-  
+          
+          output$line_chart_1 = renderPlotly(
+            {
+              #line_chart_input= input$line_input
+              Time_Series_Chart(MSO_1, input$line_view_by)
+            })
+          
+          incProgress(1/11, message = "Making Bar Chart")
+          
+          output$bar_chart_1 = renderPlotly(
+            {
+              #line_chart_input= input$line_input
+              Bar_Chart(MSO_1, input$bar_view_by)
+            })
+          
+          
+          
+          output$selection_1 <- renderPrint({
+            s_1 <- event_data("plotly_click", source = "bar_chart_click")
+            if (length(s_1) == 0) {
+              "Please click on one bar to see more details. The little chart is a range slider. You can move the views!"
+            }
+          })
+          
+          output$drilldown_bar_chart_1 = renderPlotly(
+            {
+              click_event <- event_data("plotly_click", source = "bar_chart_click")
+              Drilldown_Bar_Chart(MSO_1, click_event,
+                                  input$drilldown_bar_view_by, input$bar_view_by)
+            }
+            
+          )
+          
+          
+          incProgress(1/11, message = "Making Map")
+          output$map_1 = renderPlotly(
+            {
+              Geo_Map(clean_data, MSO_1)
+            })
+        }#end mtd1=====================================================================================================
+        if (mtd_list[[i]] == "mtd2")
+        {
+          MSO_2 = Not_Enough_OH_Method(clean_data)
+          incProgress(1/11, message = "Making Table")
+          
+          #changed from data_cleaned_2
+          output$MSO_table_2 = DT::renderDataTable(
+            DT::datatable(MSO_2, options =list(
+              pageLength = 5, paging = TRUE )))
+          
+          
+          MSO_sum_2 = sum(MSO_2$MSO)
+          
+          #changed from MSO_table_2
+          output$SalesData_2 = renderText({
+            
+            paste("Missed Sales Opportunities: $" , format(round(MSO_sum_2, digits = 0), big.mark = ",") )
+          })
+          
+          
+          output$line_chart_2 = renderPlotly(
+            {
+              #line_chart_input= input$line_input
+              Time_Series_Chart(MSO_2, input$line_view_by)
+            })
+          
+          incProgress(1/11, message = "Making Bar Chart")
+          
+          output$bar_chart_2 = renderPlotly(
+            {
+              #line_chart_input= input$line_input
+              Bar_Chart(MSO_2, input$bar_view_by)
+            })
+          
+          
+          
+          output$selection_2 <- renderPrint({
+            s_2 <- event_data("plotly_click", source = "bar_chart_click")
+            if (length(s_2) == 0) {
+              "Please click on one bar to see more details. The little chart is a range slider. You can move the views!"
+            }
+          })
+          
+          output$drilldown_bar_chart_2 = renderPlotly(
+            {
+              click_event <- event_data("plotly_click", source = "bar_chart_click")
+              Drilldown_Bar_Chart(MSO_2, click_event,
+                                  input$drilldown_bar_view_by, input$bar_view_by)
+            }
+            
+          )
+          
+          
+          incProgress(1/11, message = "Making Map")
+          output$map_2 = renderPlotly(
+            {
+              Geo_Map(clean_data, MSO_2)
+            })
+          
+        }#end mtd2=========================================================================================================
+        if(mtd_list[[i]] == "mtd3")
+        {
+          MSO_3 = Weighted_OH_Method(clean_data)
+          
+          incProgress(1/11, message = "Making Table")
+          
+          #changed from data_cleaned_3
+          output$MSO_table_3 = DT::renderDataTable(
+            DT::datatable(MSO_3, options =list(
+              pageLength = 5, paging = TRUE )))
+          
+          MSO_sum_3 = sum(MSO_3$MSO)
+          
+          #changed from sum_MSO_3
+          output$SalesData_3 = renderText({
+            
+            paste("Missed Sales Opportunities: $" , format(round(MSO_sum_3, digits = 0), big.mark = ",") )
+          })
+          
+          
+          output$line_chart_3 = renderPlotly(
+            {
+              #line_chart_input= input$line_input
+              Time_Series_Chart(MSO_3, input$line_view_by)
+            })
+          
+          incProgress(1/11, message = "Making Bar Chart")
+          
+          output$bar_chart_3 = renderPlotly(
+            {
+              #line_chart_input= input$line_input
+              Bar_Chart(MSO_3, input$bar_view_by)
+            })
+          
+          
+          
+          output$selection_3 <- renderPrint({
+            s <- event_data("plotly_click", source = "bar_chart_click")
+            if (length(s) == 0) {
+              "Please click on one bar to see more details. The little chart is a range slider. You can move the views!"
+            }
+          })
+          
+          output$drilldown_bar_chart_3 = renderPlotly(
+            {
+              click_event <- event_data("plotly_click", source = "bar_chart_click")
+              Drilldown_Bar_Chart(MSO_3, click_event,
+                                  input$drilldown_bar_view_by, input$bar_view_by)
+            }
+            
+          )
+          
+          
+          incProgress(1/11, message = "Making Map")
+          output$map_3 = renderPlotly(
+            {
+              Geo_Map(clean_data, MSO_3)
+            })
+        } #end mtd3=============================================================================================================
+         # print(head(MSO))
+
+       
+      }
+      
+      #========Cluster Methods=======================
+      
+      cluster_methods = reactive({Cluster_Data(MSO_1, MSO_2, MSO_3, input$cluster_view_by)})
+      
+      #cluster Chart of methods
+      output$cluster_chart = renderPlotly({cluster_chart = Cluster_Chart(cluster_methods(), input$cluster_view_by)})
+      
+      cluster_grid = reactive({Cluster_Grid(cluster_methods(), input$cluster_view_by)})
+      output$cluster_grid = DT::renderDataTable(
+                              DT::datatable({
+                                cluster_grid()},
+                                options =list(
+                                pageLength = 5, paging = TRUE )))
+        
     })
+  
+  
+  #=============Create .CSV file to be downloaded==============================================
+      download_data <- reactive({
+        if(input$download_opt == "down_clean_data"){clean_data}
+        else if(input$download_opt == "down_cluster"){cluster_grid()}
+        else if(input$download_opt == "down_mtd1"){MSO_1}
+        else if(input$download_opt == "down_mtd2"){MSO_2}
+        else if(input$download_opt == "down_mtd3"){MSO_3}
+      })
+    download_name <- reactive({
+      if(input$download_opt == "down_clean_data"){"Clean Data"}
+      else if(input$download_opt == "down_cluster"){"Cluster Data"}
+      else if(input$download_opt == "down_mtd1"){"Method 1 Data"}
+      else if(input$download_opt == "down_mtd2"){"Method 2 Data"}
+      else if(input$download_opt == "down_mtd3"){"Method 3 Data"}
+    })
+      output$download <- downloadHandler(
+        #name the file
+        filename = function() {
+          paste(download_name(), ".csv", sep = "")
+        },
+        content = function(file) {
+          write.csv(download_data(), file, row.names = FALSE)
+        }
+      )
   })
 }
 #-------------------
--- Shiny/ui.R
+++ Shiny/ui.R
@@ -7,10 +7,13 @@
 #-------------------
 #User Interface
 
-ui = fluidPage(
-  
+ui = fluidPage(theme = shinytheme("spacelab"),
+  #link to change themes: https://rstudio.github.io/shinythemes/
   #Title of the page
-  titlePanel("Hershey Tool"),
+  titlePanel("S.M.O.R.E. app"),
+  setBackgroundImage(src = "https://i.imgur.com/9AcGCLV.jpg"),
+    #src = "http://www.thehersheycompany.com/content/corporate/en_us/this-is-hershey/_jcr_content/pagecontent/corporatebanner/image.img.jpg/1472141151739.jpg"),
+    #src = "https://www.hersheys.com/is/image/content/dam/franchise/en_us/images/Common/hsy_tout_simpleingredients_570x320.jpg?hei=320&wid=570&fmt=jpg"),
   
   #Format Page	
   sidebarLayout(
@@ -18,40 +21,165 @@ ui = fluidPage(
     #Input Area
     sidebarPanel(
       
+      wellPanel(
       #Input .CSV file
-      fileInput("data.in", "Read Data (CSV)",
+      fileInput("data.in", "Read Data: ",
                 accept = c(
                   "text/csv",
                   "text/comma-separated-values,text/plain",
-                  ".csv"
+                  ".csv",
+                  ".xlsx"
                 )
       ),
+      selectInput("file_type", "Select File Type:",
+                 choices = c(".xlsx" = "xlsx",
+                             ".csv" = "csv",
+                             ".txt" = "txt")),
       
-      #Sliders....
-      #User interfce options
+      #Check Box for Method
+      
+      checkboxGroupInput("methods", 
+                         "Choose Methods:",
+                         c("Method 1" = "mtd1",
+                           "Method 2" = "mtd2",
+                           "Method 3" = "mtd3")),
       
       #Submit Button
-      actionButton("submit", "Submit"),
+      actionButton("submit", "Run Calculation")),
+      
+      wellPanel(
+        selectInput("cluster_view_by", "View Combined Methods by:",
+                    choices = c( "Seasonal Segmentation" = "HSY Seasonal Segmentation",
+                                 "HSY Item Description" = "HSY Item Description",
+                                 "UPC" = "UPC",
+                                 "Store Nbr" = "Store Nbr",
+                                 "Store Name" = "Store Name",
+                                 "City" = "Building City",
+                                 "State/Prov"= "Building State/Prov",
+                                 "Postal Code" = "Building Postal Code"))
+      ),
+      
+      wellPanel(
+        selectInput("line_view_by", "View Line Chart by:",
+                      choices = c("HSY Item Description" = "HSY Item Description",
+                                  "UPC" = "UPC",
+                                  "Seasonal Segmentation" = "HSY Seasonal Segmentation",
+                                  "Store Nbr" = "Store Nbr",
+                                  "Store Name" = "Store Name",
+                                  "City" = "Building City",
+                                  "State/Prov"= "Building State/Prov",
+                                  "Postal Code" = "Building Postal Code"))),
+      wellPanel(
+      selectInput("bar_view_by", "View Bar Chart by:",
+                  choices = c("State/Prov" = "Building State/Prov",
+                              "HSY Item Description" = "HSY Item Description",
+                              "UPC" = "UPC",
+                              "Seasonal Segmentation" = "HSY Seasonal Segmentation",
+                              "Store Nbr" = "Store Nbr",
+                              "Store Name" = "Store Name",
+                              "City" = "Building City",
+                              "Postal Code" = "Building Postal Code")),
+      
+      selectInput("drilldown_bar_view_by", "Drill Down by:",
+                  choices = c("Postal Code" = "Building Postal Code",
+                              "State/Prov" = "Building State/Prov",
+                              "HSY Item Description" = "HSY Item Description",
+                              "UPC" = "UPC",
+                              "Seasonal Segmentation" = "HSY Seasonal Segmentation",
+                              "Store Nbr" = "Store Nbr",
+                              "Store Name" = "Store Name",
+                              "City" = "Building City"))),
+ 
+      selectInput("download_opt", "Select Download:",
+                  choices = c("Clean Data" = "down_clean_data",
+                              "Cluster Methods"= "down_cluster",
+                              "Method 1" = "down_mtd1",
+                              "Method 2" = "down_mtd2",
+                              "Method 3"= "down_mtd3")),
       
       #Download finished data
-      downloadButton("download", "Download"),
-      
+     # downloadButton("download", "Download Clean Data"),
+    downloadButton("download", "Download"),
       #fix width to 4
       width = 4
     ),
     
     #Ouput Area
     mainPanel(
+      #Add conditionalpanel() for all 3 methods
+          tabsetPanel(
+            tabPanel("Cluster data",
+                     br(),
+                     br(),
+                      DT::dataTableOutput("cluster_grid"),
+                     br(),
+                     br(),
+                     br(),
+                        plotlyOutput("cluster_chart")
+                     ),
+           
+            tabPanel("Charts Mtd 1",
+              h2(textOutput("sum_MSO_1")),
+              #Display Browsed Data
+              plotlyOutput("line_chart_1"),
+              plotlyOutput("bar_chart_1"),
+              verbatimTextOutput("selection_1"),
+              plotlyOutput("drilldown_bar_chart_1")),
+            
+            tabPanel( "Data Table Mtd 1",
+              h2(textOutput("SalesData_1")),
+              br(),
+              br(),
+              DT::dataTableOutput("MSO_table_1")
+              ),
+            tabPanel('Map Mtd 1',
+                     plotlyOutput("map_1")),
+            #====================Tab2==========================================
+    
+         tabPanel("Charts Mtd 2",
+                  h2(textOutput("sum_MSO_2")),
+                  #Display Browsed Data
+                  plotlyOutput("line_chart_2"),
+                  plotlyOutput("bar_chart_2"),
+                  verbatimTextOutput("selection_2"),
+                  plotlyOutput("drilldown_bar_chart_2")),
+         
+         tabPanel( "Data Table Mtd 2",
+                   h2(textOutput("SalesData_2")),
+                   br(),
+                   br(),
+                   DT::dataTableOutput("MSO_table_2")
+         ),
+         tabPanel('Map Mtd 2',
+                  plotlyOutput("map_2")),
+         
+         #==============Tab3=============================================
+         tabPanel("Charts Mtd 3",
+                  br(),
+                  h2(textOutput("sum_MSO_3")),
+                  #Display Browsed Data
+                  plotlyOutput("line_chart_3"),
+                  br(),
+                  plotlyOutput("bar_chart_3"),
+                  verbatimTextOutput("selection_3"),
+                  br(),
+                  plotlyOutput("drilldown_bar_chart_3")),
+         
+         tabPanel( "Data Table Mtd 3",
+                   br(),
+                   h2(textOutput("SalesData_3")),
+                   br(),
+                   br(),
+                   DT::dataTableOutput("MSO_table_3")
+                   #h2(textOutput("MSO_by_Item"))
+         ),
+         tabPanel('Map Mtd 3',
+                  plotlyOutput("map_3")
+         ))
       
-      #Display Browsed Data
-      h2(textOutput("SalesData")),
-      #DT::dataTableOutput("data.out"),
-      
-      DT::dataTableOutput("data_cleaned"),
-      h2(textOutput("sum_MSO"))
-      
+      )#end main panel
       
     )
   )
-)
+
 #-------------------
